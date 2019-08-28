6891adb8bad8b942ca43969cc04e5a6bd68c6f18
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-08-28 23:10:49 +0500

    adds the registred method in Register controller and genrateToken in User model

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 })->name('user.show');
 
+Route::post('register', 'Auth\RegisterController@register')->name('user.register');
+
 Route::resource('article', 'ArticleController', ['except' => ['edit', 'create']]);
