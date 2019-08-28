c85674014b5e42cfe937d451a047a75c75fe423a
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-08-28 22:51:23 +0500

    adds the json responce in exception handler to send when resource not found

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,6 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+})->name('user.show');
 
 Route::resource('article', 'ArticleController', ['except' => ['edit', 'create']]);
